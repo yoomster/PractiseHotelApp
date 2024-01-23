19af901475ec40d5557960d7e838a3af171dbbb5
yoomster <nperenboom@hotmail.com>
2024-01-23 14:26:53 +0100

    Add project files.

---
+++ PractiseHotel/Models/RoomModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PractiseHotel.Models
+{
+    public class RoomModel
+    {
+        public int Id { get; set; }
+        public string RoomType { get; set; }
+        public bool IsAvailible { get; set; } = false;
+    }
+}
